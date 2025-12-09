e42ee1d95a3a57dbdaaa2c453c449cd2b3d9e823
Ray Yang <ray.yang150701@gmail.com>
2025-12-09 05:38:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-751317964-5794f",
+  "appId": "1:825038514:web:cf414cd3f6b84d752adbab",
+  "apiKey": "AIzaSyDn0dDZJ3F_t-fEtmpNkkF-JsKPR7J6fXk",
+  "authDomain": "studio-751317964-5794f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "825038514"
+};
