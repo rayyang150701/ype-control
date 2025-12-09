6b43fcabbbc59c71586a790a4b6de0d066a58046
Ray Yang <ray.yang150701@gmail.com>
2025-12-09 06:42:56 +0000

    Try fixing this error: `Runtime SyntaxError: Expected property name or '

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,32 +1,23 @@
 import { initializeApp, getApps, cert, getApp } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
 import { Auth, getAuth } from 'firebase-admin/auth';
+import serviceAccount from '../../docs/service-account.json';
 
 let db: Firestore;
 let auth: Auth;
 
-if (!getApps().length) {
+if (getApps().length === 0) {
   try {
-    if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
-      const serviceAccount = JSON.parse(
-        process.env.FIREBASE_SERVICE_ACCOUNT_KEY
-      );
-
-      initializeApp({
-        credential: cert({
-          projectId: serviceAccount.project_id,
-          clientEmail: serviceAccount.client_email,
-          privateKey: serviceAccount.private_key?.replace(/\\n/g, '\n'),
-        }),
-      });
-    } else {
-      throw new Error(
-        'Firebase service account key is not set in the environment variables. Please set FIREBASE_SERVICE_ACCOUNT_KEY.'
-      );
-    }
+    initializeApp({
+      credential: cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key.replace(/\\n/g, '\n'),
+      }),
+    });
   } catch (error) {
     console.error('Firebase admin init error:', error);
-    throw error;
+    throw error; // Re-throw the error to halt execution if initialization fails
   }
 }
 
