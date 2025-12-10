e37e30702802013870db283dc8b8ecb7f6a32cd2
Ray Yang <ray.yang150701@gmail.com>
2025-12-10 00:16:57 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/dashboard/edit-project-dialog.tsx
+++ src/components/dashboard/edit-project-dialog.tsx
@@ -102,7 +102,7 @@ export function EditProjectDialog({ isOpen, setIsOpen, project, onProjectUpdated
         })),
       });
     }
-  }, [project, isOpen, reset]);
+  }, [project, isOpen]);
 
 
   useEffect(() => {
